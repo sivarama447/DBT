09153b3030b979edc8f1b440dac9d60b7a6f222d
thotasivaramakrishna8@gmail.com <thotasivaramakrishna8@gmail.com>
2024-11-06 06:40:04 +0000

    added 2 more columns

--- models/destination_players_table.sql
+++ models/destination_players_table.sql
@@ -2,7 +2,7 @@
 
 with source_data as (
 
-    select P_ID,P_NAME,p_team,p_score,p_wickets from DEV_DB.PUBLIC.PLAYERS_TABLE
+    select P_ID,P_NAME,p_team,p_score,p_wickets,NO_OF_BALLS_PLAYED,NO_OF_BALLS_DELIVERED from DEV_DB.PUBLIC.PLAYERS_TABLE
 )
 
 select *
